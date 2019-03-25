4ebad64425ff900eac72542ed96f08c04ddeab1f
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-03-25 12:03:43 -0700

    new files

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,25 @@
 //Import the MySQL connection.
 var connection = require('../config/connection');
 
+//Create
+
+//End Create
+
+
+//Read 
+
+//End Read
+
+
+//Update
+
+//End Update
+
+
+//Delete
+
+//End Delete
+
+
 //Export the orm object for the model
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-var express = require('express');
+var express = require("express");
+
+var PORT = process.env.PORT  || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+//Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Import Burger controllers and give the server access
+var routes = require("./controllers/burgers_controller");
+
+app.use("/",routes);
+
+//Start server so that it can begin listening to the client
+app.listen(PORT);
+//Log when server has started
+console.log("Listinging on port: " + PORT);
