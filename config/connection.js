7196463f38f955faf751a37e83906552e11c30ce
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-03-25 11:08:12 -0700

    skeleton

--- config/connection.js
+++ config/connection.js
@@ -3,24 +3,25 @@ require('dotenv').config();
 //Set up MySQL connection.
 const mysql = require('mysql');
 
-var keys = require('./keys.js');
+//var keys = require('./keys.js');
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'keys.secret',
-    //password: '93PennyDog!',
+    // password: keys.secret,
+    password: '93PennyDog!',
     database: 'burgers_db',
     port: 3306
 });
 
-console.log(connection);
+//console.log(connection);
+
 //Make connection
 connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
-    }
+    };
     console.log('connected as id: ' + connection.threadId);
 });
 
