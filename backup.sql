a150116f15c5d60c578cf7cd37b8fa6527228f12
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-03-27 22:41:04 -0700

    add back up

---
+++ backup.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.17  Distrib 10.3.13-MariaDB, for osx10.14 (x86_64)
+--
+-- Host: localhost    Database: burgers_db
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `burgers`
+--
+
+DROP TABLE IF EXISTS `burgers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `burgers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(200) NOT NULL,
+  `devoured` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `burgers`
+--
+
+LOCK TABLES `burgers` WRITE;
+/*!40000 ALTER TABLE `burgers` DISABLE KEYS */;
+INSERT INTO `burgers` VALUES (1,'Veggie Burger',1),(2,'Blackened Chicken Burger',1),(3,'Teriyaki Burger',1),(4,'Test Burger',1),(5,'Flying Burger',1),(6,'Burger',1),(7,'Joking Burger',1),(8,'Bk burger',1),(9,';lkj',1),(10,'Tester Burger',0);
+/*!40000 ALTER TABLE `burgers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-27 21:45:15
