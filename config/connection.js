5f4c674b761749c0f7b8d1827e707dc741ab1e83
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-03-27 18:18:16 -0700

    Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ const mysql = require('mysql');
 
 var keys = require('../config/keys.js');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: keys.secret,
-    database: 'burgers_db',
-    port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: keys.secret,
+        database: 'burgers_db',
+        port: 3306
+    });
+};
 
 //Make connection
 connection.connect(function (err) {
